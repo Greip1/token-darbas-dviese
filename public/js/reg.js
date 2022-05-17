2794a7e204e41d56b1cc80b98f187575ac053b1f
Greta <gretaapul@gmail..com>
2022-05-17 15:00:56 +0300

    front errors handled v1

--- public/js/reg.js
+++ public/js/reg.js
@@ -3,6 +3,9 @@ const baseUrl = 'http://localhost:3000';
 
 const errroEl = document.getElementById('err');
 
+const emailErr = document.getElementById('emailError');
+const passErr = document.getElementById('passError');
+
 formEl.addEventListener('submit', async (event) => {
   event.preventDefault();
   console.log('js submit form');
@@ -18,17 +21,37 @@ formEl.addEventListener('submit', async (event) => {
     handleError('data incorrect: password error');
     return;
   }
+  if (formEl.elements.password.value.length < 6) {
+    handleError('data incorrect: password is too short');
+    return;
+  }
   registerFetch(regObj.email, regObj.password);
 });
 
-function handleError(msg) {
+function handleError(msgArrErr) {
   errroEl.textContent = '';
-  if (typeof msg === 'string') {
-    errroEl.textContent = msg;
+
+  if (typeof msgArrErr === 'string') {
+    errroEl.textContent = msgArrErr;
   }
-  if (Array.isArray(msg)) {
-    msg.forEach((errorOb) => {
-      errroEl.innerHTML += `${errorOb.message}<br>`;
+  if (Array.isArray(msgArrErr)) {
+    passErr.innerHTML = '';
+    emailErr.innerHTML = '';
+    msgArrErr.forEach((errorOb) => {
+      console.log('errorOb.path', errorOb.path[0]);
+      if (errorOb.path[0] === 'email') {
+        const pEl = document.createElement('p');
+        pEl.textContent = errorOb.message;
+        emailErr.append(pEl);
+      }
+      if (errorOb.path[0] === 'password') {
+        const pEl = document.createElement('p');
+
+        pEl.textContent = errorOb.message;
+        passErr.append(pEl);
+      }
+
+      //   dest.innerHTML += `${errorOb.message}<br>`;
     });
   }
 }
@@ -47,6 +70,8 @@ async function registerFetch(email, password) {
     handleError('registration successful');
   } else {
     // fail
-    handleError(await resp.json());
+    const res = await resp.json();
+    console.log(res);
+    handleError(res);
   }
 }
